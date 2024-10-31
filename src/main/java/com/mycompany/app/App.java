153df82f890f5ad21ff3fbe8540a1f5ec5b8f1c4
curso00 <curso00@nomail.com>
2024-10-31 12:14:45 +0100

    Nombres de variables, referencias, creación de instancias y comentarios

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,15 +4,28 @@ package com.mycompany.app;
  * Hello world!
  */
 
-public class Pepe {
+public class App {
     private int a = 3;
 
-    
     public static void main(String[] args) {
-        
+
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+
+        Juan miInstanciJuan = new Juan();
+
+        Object variableObject = new App();
+        // Esto no
+
+        // Juan variableJuan = new App();
+
+        /*
+         * Esto no
+         * Esto tampoco
+         */
+
+        System.out.println(miInstanciJuan);
     }
 }
